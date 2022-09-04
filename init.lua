338c387c7ee941e8e07a9e6b92cdba543108cbb7
infinitebash <32340104+infinitebash@users.noreply.github.com>
2022-09-04 03:26:10 -0700

    imagine i remembered to git add before the last commit

--- init.lua
+++ init.lua
@@ -18,6 +18,9 @@ vim.opt.number = true
 vim.opt.termguicolors = true
 vim.opt.linebreak = true
 
+-- Quietly initialize COQ
 vim.api.nvim_create_autocmd({"VimEnter"}, { pattern = "*", command = "COQnow --shut-up"})
+-- Open CHADTree without focus
 vim.api.nvim_create_autocmd({"VimEnter"}, { pattern = "*", command = "CHADopen --nofocus"})
+-- Close nvim on quit (required because of CHADTree)
 vim.api.nvim_create_autocmd({"BufEnter"}, { pattern = "*", command = "if (winnr(\"$\") == 1 && &filetype == \"CHADTree\") | q | endif" })
